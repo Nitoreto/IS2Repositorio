aecce0ff52b40475531fb3f76005621f9b8e33e6
fmart <felmar03@ucm.es>
2021-06-07 01:54:42 +0200

    DAOCLiente

--- PcComponentesUchuvaFinal/src/DAO_Conexion/DAOClientes.java
+++ PcComponentesUchuvaFinal/src/DAO_Conexion/DAOClientes.java
@@ -7,27 +7,27 @@ import factoria.Cliente;
 public class DAOClientes {
 	private String DNI;
 	private Conexion conexion;
-	
+
 	public DAOClientes(Conexion conexion) {
 		super();
 	}
 
 	public String Buscar() {
 		try {
-			this.query = "SELECT *FROM clientes WHERE DNI = '" + cliente.getDNI() + "'";
-			this.transfer = new Transfer( super.conectarExecute());
+			this.query = "SELECT * FROM Cliente WHERE DNI = '" + cliente.getDNI() + "'";
+			this.transfer = new Transfer(super.conectarExecute());
 		} catch (SQLException e) {
 			return e.getMessage();
 		}
 		return "Exito";
 
 	}
-	
+
 	public String Crear() {
 		try {
 			this.DNI = cliente.getDNI();
-			this.query = "INSERT into clientes (`Nombre`, `DNI`, `Telefono`) VALUES " + "('" + cliente.getNombre()
-					+ "', '" + cliente.getDNI() + "', '" + cliente.getTelefono() + "')";
+			this.query = "INSERT into Cliente (DNI, Nombre, Telefono) VALUES " + "('" + cliente.getDNI() + "', '"
+					+ cliente.getNombre() + "', '" + cliente.getTelefono() + "')";
 			if (DNI.equals("")) {
 				throw new Exception("Campo dni esta vacio");
 
@@ -40,14 +40,15 @@ public class DAOClientes {
 		}
 		return "Exito";
 	}
-	
+
 	public String Eliminar() {
 		int row = -1;
 		try {
-			this.query = "DELETE  FROM clientes WHERE DNI = '" + cliente.getDNI() + "'";
+			this.query = "DELETE  FROM Cliente WHERE DNI = '" + cliente.getDNI() + "'";
 			row = this.conectarUpdate();
 		} catch (SQLException e) {
-			if (e.getClass().getName().equals("com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException")) {
+			if (e.getClass().getName()
+					.equals("com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException")) {
 				return "No se puede borrar un cliente con ventas";
 			}
 			return e.getMessage();
@@ -58,11 +59,11 @@ public class DAOClientes {
 		return "Exito";
 
 	}
-	
+
 	public String Desactivar() {
 		int row = -1;
 		try {
-			this.query = "UPDATE clientes SET Activo = 0 WHERE DNI = '" + cliente.getDNI() + "'";
+			this.query = "UPDATE Cliente SET Activo = 0 WHERE DNI = '" + cliente.getDNI() + "'";
 			row = super.conectarUpdate();
 		} catch (Exception e) {
 
@@ -73,30 +74,30 @@ public class DAOClientes {
 		}
 		return "Exito";
 	}
-	
-    public String Listar() {
 
+	public String Listar() {
 
 		try {
-			this.query = "SELECT * FROM clientes";
+			this.query = "SELECT * FROM Cliente";
 			this.transfer = new Transfer(super.conectarExecute());
-			
+
 		} catch (SQLException e) {
 			return e.getMessage();
 		}
-		
+
 		return "Exito";
-	}   
-    
+	}
+
 	public String Modificar() {
 		int row = -1;
 		try {
-			this.query = "UPDATE clientes SET nombre = '" + cliente.getNombre() + "', DNI = '" + cliente.getDNI()
-			+ "', telefono = " + cliente.getTelefono() + ", Activo = " + cliente.getActivo() + " WHERE DNI = '"
-			+ DNI + "'";
+			this.query = "UPDATE Cliente SET DNI = '" + cliente.getDNI() + "', Nombre = '" + cliente.getNombre()
+					+ "', telefono = " + cliente.getTelefono() + ", Activo = " + cliente.getActivo() + " WHERE DNI = '"
+					+ DNI + "'";
 			row = super.conectarUpdate();
 		} catch (Exception e) {
-			if (e.getClass().getName().equals("com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException")) {
+			if (e.getClass().getName()
+					.equals("com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException")) {
 				return "No se puede modificar el DNI de un cliente con ventas";
 			}
 			return e.getMessage();
@@ -106,6 +107,5 @@ public class DAOClientes {
 		}
 		return "Exito";
 	}
-	
 
 }
