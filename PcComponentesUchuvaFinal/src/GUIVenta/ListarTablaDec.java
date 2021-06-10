982f91037a060a7585a0187f70477917983654a3
Ana <Ana@DESKTOP-KS3VVU7>
2021-06-10 21:24:27 +0200

    avances

--- PcComponentesUchuvaFinal/src/GUIVenta/ListarTablaDec.java
+++ PcComponentesUchuvaFinal/src/GUIVenta/ListarTablaDec.java
@@ -1,6 +1,10 @@
 package GUIVenta;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +13,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 
+import GUISucursal.BorderDecorator;
+import GUISucursal.backGroundDecorator;
 import main.Mediator;
 
 public class ListarTablaDec extends JFrame{
@@ -27,6 +33,48 @@ public class ListarTablaDec extends JFrame{
 			initComponentes();
 		
 	}
+	
+	
+	private void initComponentes() {
+		backGroundDecorator backGround = new backGroundDecorator();
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+		backGround.setLayout(new BorderLayout());
+		BorderDecorator mainPanel = new BorderDecorator();
+		backGround.add(mainPanel, BorderLayout.CENTER);
+		this.setContentPane(backGround);
+
+		textoListarVentas = new JTextArea();
+		botonOk = new JButton();
+		
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(1920, 1080));
+
+		textoListarVentas.setEditable(false);
+		textoListarVentas.setText("Lista de Ventas");
+		textoListarVentas.setFocusable(false);
+		textoListarVentas.setFont(new Font("Consolas", 2, 100));
+		textoListarVentas.setBackground(Color.green);
+		textoListarVentas.setForeground(Color.white);
+		mainPanel.add(textoListarVentas, BorderLayout.NORTH);
+
+		botonOk.setText("Cancelar");
+		botonOk.setFont(new Font("Consolas", 4, 80));
+		botonOk.setForeground(Color.red);
+		botonOk.setContentAreaFilled(false);
+		botonOk.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				botonOkActionPerformed();
+			}
+		});
+		mainPanel.add(botonOk, BorderLayout.SOUTH);
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+
 	private String CrearTabla() {
 		String[] datos = { "rand", DNICliente };
 		String inf = controlador.mostrarHistorial("ControllerVenta", datos);
@@ -43,6 +91,15 @@ public class ListarTablaDec extends JFrame{
 			this.getContentPane().add(paneScroll, BorderLayout.CENTER);
 			this.validate();
 			return "Exito";
+		}
+	}
+	private void botonOkActionPerformed() {
+		String inf = controlador.cancelar();
+		if (inf != "Exito") {
+			JOptionPane.showMessageDialog(null, "Error: " + inf, "ERROR AL Modificar", JOptionPane.ERROR_MESSAGE);
+		} else {
+			this.dispose();
+			new PantallaPrincipalVentas(controlador);
 		}
 	}
 
